ceccb2d4cad34ec20ade524036f19657e2a024b9
anupn <anup.natta@gmail.com>
2023-02-25 15:33:50 +0530

    test

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,10 +1,8 @@
- resource "aws_instance" "ec2" {
-   ami = "ami-0a017d8ceb274537d"
-   instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
-    tags = {
-       name="test"
-    }
-
-
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags = {
+    Name = "test"
+  }
 }
