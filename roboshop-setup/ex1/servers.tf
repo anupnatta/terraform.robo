987d49ff3987e49c96ade153be74c0d28f153d6f
anupn <anup.natta@gmail.com>
2023-02-26 19:11:20 +0530

    ec2 instances + DNS Record

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -14,3 +14,153 @@ resource "aws_route53_record" "frontend" {
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
+
+resource "aws_instance" "user" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "user-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "cart-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+resource "aws_instance" "shipping" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "shipping-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "mysql-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "mongodb-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "redi-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+resource "aws_instance" "catalogue" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "catalogue-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+resource "aws_instance" "payment" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "payment-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z062643811DGQBMMY8AIZ"
+  name    = "rabbitmq-dev.devopsamn.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
