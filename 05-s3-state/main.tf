d3ac6faf689a643f26223dcacc56fd2ab0f10066
anupn <anup.natta@gmail.com>
2023-02-27 20:55:19 +0530

    terraform state file

---
+++ 05-s3-state/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terrafrom-ab71"
+    key    = "05-s3-state.terraform-ab71"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-01a1fb86f66c91c21"]
+  tags = {
+    Name = "s3bucket"
+  }
+
+}
